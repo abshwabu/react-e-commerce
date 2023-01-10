586eb657cde8e67b6dae23be70582ce5cf44b9d8
ashwabu <abdulazizredi90@gmail.com>
2023-01-10 21:52:41 +0300

    testimonial finished

--- src/components/testimonial/Testimonial.js
+++ src/components/testimonial/Testimonial.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import css from './Testimonial.module.css'
 import Hero from '../../assets/testimonialHero.png'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { TestimonialsData } from '../../data/testimonials'
 
 function Testimonial() {
     return (
-        <div className={css.testimonial}>
+        <div className={css.testimonials}>
             <div className={css.wrapper}>
                 <div className={css.container}>
                     <span>Top Rated</span>
@@ -17,6 +19,29 @@ function Testimonial() {
                     <span>100k+</span>
                     <span>Happy Customer </span>
                 </div>
+            </div>
+            <div className={css.reviews}>Reviews</div>
+            <div className={css.carousel}>
+                <Swiper
+                    slidesPerView={3}
+                    slidesPerGroup={1}
+                    spaceBetween={20}
+                    className={css.tCarousel}
+                >
+                    {
+                        TestimonialsData.map((testimonial, i) => (
+                            <SwiperSlide>
+                                <div className={css.testimonial}>
+                                    <img src={testimonial.image} alt="" />
+                                    <span>{testimonial.comment}</span>
+                                    <hr />
+                                    <span>{testimonial.name}</span>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
+
             </div>
         </div>
     )
