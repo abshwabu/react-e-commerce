0aa958c3781360a81b3ad7bd7c7fa52bbed898ac
ashwabu <abdulazizredi90@gmail.com>
2023-01-11 08:45:59 +0300

    footer finished

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import css from './Footer.module.css'
 import Logo from '../../assets/logo.png'
-import {ImboxIcon,PhoneIcon,LocationMarkerIcon,LoginIcon,UserIcon,LinkIcon} from '@heroicons/react/outline'
+import {InboxIcon,PhoneIcon,LocationMarkerIcon,LoginIcon,UserIcon,LinkIcon} from '@heroicons/react/outline'
 
 function Footer() {
   return (
@@ -20,8 +20,51 @@ function Footer() {
                   <LocationMarkerIcon className={css.icon}/>
                   <span>111 north avenue Orlando, FL 32001</span>
                 </span>
+                <span className={css.pngLine}>
+                  <PhoneIcon className={css.icon}/>
+                  <span>+1234567890</span>
+                </span>
+                <span className={css.pngLine}>
+                  <InboxIcon className={css.icon}/>
+                  <span>16386</span>
+                </span>
               </div>
             </div>
+            <div className={css.block}>
+              <div className={css.detail}>
+                <span>Account</span>
+                <span className={css.pngLine}>
+                  <LoginIcon className={css.icon}/>
+                  <span>Login</span>
+                </span>
+                
+              </div>
+            </div>
+            <div className={css.block}>
+              <div className={css.detail}>
+                <span>Company</span>
+                <span className={css.pngLine}>
+                  <UserIcon className={css.icon}/>
+                  <span>About Us</span>
+                </span>
+               
+              </div>
+            </div>
+            <div className={css.block}>
+              <div className={css.detail}>
+                <span>Contact Us</span>
+                <span className={css.pngLine}>
+                  <LinkIcon className={css.icon}/>
+                  <span>Term & Policy</span>
+                </span>
+               
+              </div>
+            </div>
+            
+        </div>
+        <div className={css.copyRight}>
+          <span>Copyright &copy;2023 by E-commerce, Inc </span>
+          <span>All rights reserved</span>
         </div>
     </div>
   )
