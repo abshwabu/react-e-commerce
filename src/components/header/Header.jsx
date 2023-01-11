9d7b7c4ff742a7e1aea219b3e0f18076ccf90fdc
ashwabu <abdulazizredi90@gmail.com>
2023-01-11 13:43:45 +0300

    name changed

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import css from './Header.module.css'
+import logo from '../../assets/logo.png'
+import {CgShoppingBag} from 'react-icons/cg'
+import {GoThreeBars} from 'react-icons/go'
+
+function Header() {
+  return (
+    <div className={css.container}>
+        <div className={css.logo}>
+            <img src={logo} alt="logo" />
+            <span>E-commerce</span>
+        </div>
+        <div className={css.right}>
+          <div className={css.bars}>
+            <GoThreeBars/>
+          </div>
+            <div className={css.menu}>
+              <ul className={css.menu}>
+                <li>Collection</li>
+                <li>Brands</li>
+                <li>New</li>
+                <li>Sales</li>
+                <li>Eng</li>
+              </ul>
+            </div>
+            <input type="text" className={css.search} placeholder="Search" />
+
+           <CgShoppingBag className={css.cart}/>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
